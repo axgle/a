c99584bb85f8c86ff2f19c2f0f6ea2a506e6eae1
Nick Roma <nikoandpiko@gmail.com>
2021-04-13 01:08:53 +0900

    3rd anniversary update

--- rakefile.rb
+++ rakefile.rb
@@ -1,25 +1,22 @@
 task :default do
-	puts `gem build a.gemspec`
-	puts `gem install --local #{latest_gem}`
-    puts  `ruby -e '' -ra`
-	puts `a gem mytest`
+  puts `gem build a.gemspec`
+  puts `gem install --local #{latest_gem}`
+  puts `ruby -e '' -ra`
+  puts `a gem mytest`
 end
 
 task :install do
-	puts `gem install --local #{latest_gem}`
+  puts `gem install --local #{latest_gem}`
 end
 
 task :push do
-    puts "pusing #{latest_gem}..."
-    exec "gem push #{latest_gem}"
-    if $?.success?
-        puts "https://rubygems.org/gems/a"
-    end
-end    
+  puts "pusing #{latest_gem}..."
+  exec "gem push #{latest_gem}"
+  puts 'https://rubygems.org/gems/a' if $CHILD_STATUS.success?
+end
 
-task :i=>:install
-
+task i: :install
 
 def latest_gem
-    Dir["*.gem"].sort.last
+  Dir['*.gem'].max
 end
