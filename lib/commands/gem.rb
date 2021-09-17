7e44f53e1ccfa9d5bd15eb3f566cc25855f5c707
neet93 <gehirnmav7@protonmail.com>
2021-09-17 03:34:29 -0400

    refactored gem.rb

--- lib/commands/gem.rb
+++ lib/commands/gem.rb
@@ -1,105 +1,62 @@
+require 'fileutils'
+
 module A
-    class Gem
-        class << self
+  class Gem
 
-            def make_dirs(config)
-                dir = config[:dir]
-               
-               Dir.mkdir dir rescue nil
+    BINFILE = %[#!/usr/bin/env ruby
+puts 'Hello, world!'
+]
 
-               Dir.chdir dir
-               Dir.mkdir 'lib' rescue nil
-               Dir.mkdir 'bin' rescue nil
-            end
+RAKEFILE = %[desc 'Used for quick help on terminal'
+task :foo
+  sh "echo 'foo'"
+end]
 
-            def make_rakefile(config)
-                dir = config[:dir]
-               
-                rakefile = <<-eot
-def latest_gem
-    Dir["*.gem"].sort.last
-end 
+GEMSPEC = %[Gem::Specification.new do |s|
+  s.name = 'a'
+  s.version = "0.1.0"
+  s.summary = "summary"
+  s.authors = ['authors']
 
-task :default do
-    puts `gem build #{dir}.gemspec`
-    if $?.success?
-        puts "\\nyou can install it with:"
-        puts "rake install\\n"
-        puts "then run your app with:\\n#{dir}\\n"
-            
-    end    
+  s.license = 'MIT'
+  s.email = 'degcat@126.com'
+  s.homepage = 'https://rubygems.org/gems
+
+  s.files = Dir["lib/**/*.rb"]
+  s.executables << 'a'
 end
-task :run do
-    puts `gem build #{dir}.gemspec`
-    puts `gem install --local \#{latest_gem}`
-    puts "===output==="
-    puts `#{dir}`
-    
-end
+]
 
-task :install do
-    puts `gem install --local \#{latest_gem}`
-end
+    def initialize(config)
+      dir = config[:dir]
+      # Is this used anywhere?
+      target = config[:target]
 
-task :push do
-    puts "pusing \#{latest_gem}..."
-    puts `gem push \#{latest_gem}`
-    if $?.success?
-        puts "https://rubygems.org/gems/#{dir}"
+      make_dirs(config)
+      make_rakefile(config)
+      make_specfile(config)
+      File.write("#{dir}/lib/#{dir}.rb", '')
+      File.write("#{dir}/bin/#{dir}", BINFILE)
+      FileUtils.chmod(0755, "#{dir}/bin/#{dir}") rescue nil
     end
-end    
 
-task :i=>:install
+    def make_dirs(config)
+      dir = config[:dir]
 
-eot
-                IO.write "rakefile.rb",rakefile
-            end
-
-            def make_specfile(config)
-                dir = config[:dir]
-                gemspec = <<-eot
-
-Gem::Specification.new do |s|
-    s.name = "#{dir}"
-    s.version = "0.0.1"
-    s.summary = "summary"
-    s.authors = ['authors']
-
-    s.license = 'MIT'
-    s.email = 'degcat@126.com'
-    s.homepage = 'https://rubygems.org/gems/a'
-
-    s.files =  Dir["lib/*.rb"]
-    s.executables << "#{dir}" 
-end
-
-eot
-              IO.write "#{dir}.gemspec",gemspec
-            end
-            def index(config)
-                dir = config[:dir]
-                target = config[:target]
-
-                self.make_dirs(config)
-
-                self.make_rakefile(config)
-                self.make_specfile(config)
-        
-                IO.write "lib/#{dir}.rb",''
-
-                binfile=<<-eot
-#!/usr/bin/env ruby                
-puts "hello world!"
-eot
-                IO.write "bin/#{dir}",binfile
-
-                FileUtils.chmod "u=wrx","bin/#{dir}" rescue nil
- 
-
-
-#puts gemspec
-
-            end
-        end
+      paths = ["#{dir}/lib", "#{dir}/bin"]
+      paths.each { |p| FileUtils.mkdir_p(p) }
     end
+
+    def make_rakefile(config)
+      dir = config[:dir]
+
+      File.write("#{dir}/Rakefile", RAKEFILE)
+    end
+
+    def make_specfile(config)
+      dir = config[:dir]
+
+      File.write("#{dir}/#{dir}.gemspec", GEMSPEC)
+    end
+  end
 end
